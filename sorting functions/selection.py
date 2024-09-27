a86bba9c814222da7f86b1209491642f210fe165
sam-rico7 <samuel.rico@ucas-edu.net>
2024-09-27 17:13:04 +0000

    fixed time and added # of swaps

--- sorting functions/selection.py
+++ sorting functions/selection.py
@@ -1,20 +1,26 @@
 import time
 
+nums = [5, 9, 10, 12, 13, 7, 5]
+print("Original list:", nums)
 
+start = time.time()
 
-nums = [17, 30, 41, 15, 7, 12, 23, 25, 12]
+swap_count = 0
 
-for i in range(len(nums)-1):
-    
-
+for i in range(len(nums) - 1):
     min_idx = i
-    for j in range(i+1, len(nums)):
+    for j in range(i + 1, len(nums)):
         if nums[min_idx] > nums[j]:
             min_idx = j
-    
-    nums[i], nums[min_idx] = nums[min_idx], nums[i]
-    print(f"Min Number {nums[i]}")
+            
+    if min_idx != i:
+        nums[i], nums[min_idx] = nums[min_idx], nums[i]
+        swap_count += 1  # Increment the swap counter
+        print(f"Swapped indices {i} and {min_idx}: {nums}")
 
-print ("Sorted array")
-for i in range(len(nums)):
-    print(nums[i],end=" ")
+end = time.time()
+
+print("Sorted array:", nums)
+print(f"\nTotal swaps: {swap_count}")
+print("Time: " + str(int((end-start)*1000)) + " milliseconds")
+
