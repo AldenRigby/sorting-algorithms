11fb4092414a280c7b3086fa3644c6b06585dd9d
sam-rico7 <samuel.rico@ucas-edu.net>
2024-09-27 17:18:35 +0000

    added comments

--- sorting functions/selection.py
+++ sorting functions/selection.py
@@ -1,23 +1,31 @@
+#made by Samuel Rico Rodriguez Correa Melo Rey Reyes.
+
 import time
 
+#original array
 nums = [5, 9, 10, 12, 13, 7, 5]
+
+#print original array
 print("Original list:", nums)
 
+#start the timer
 start = time.time()
 
+#create swap count variable that tracks the swaps in the sorting process
 swap_count = 0
 
-for i in range(len(nums) - 1):
-    min_idx = i
-    for j in range(i + 1, len(nums)):
-        if nums[min_idx] > nums[j]:
-            min_idx = j
+for i in range(len(nums) - 1): #iterate through the list except the last one
+    min_idx = i #initialize min_idx
+    for j in range(i + 1, len(nums)): #do if new smalles element is found
+        if nums[min_idx] > nums[j]: #check smallest element
+            min_idx = j #updates smallest element
             
     if min_idx != i:
-        nums[i], nums[min_idx] = nums[min_idx], nums[i]
+        nums[i], nums[min_idx] = nums[min_idx], nums[i] #swap elements
         swap_count += 1  # Increment the swap counter
         print(f"Swapped indices {i} and {min_idx}: {nums}")
 
+#stop the timer
 end = time.time()
 
 print("Sorted array:", nums)
