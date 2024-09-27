3204d3f594cb8399433fddc590f45b2be70608fe
AldenRigby <alden.rigby@ucas-edu.net>
2024-09-27 11:00:59 -0600

    resolve merge conflict

--- sorting functions/selection.py
+++ sorting functions/selection.py
@@ -11,13 +11,9 @@ for i in range(len(nums)-1):
     for j in range(i+1, len(nums)):
         if nums[min_idx] > nums[j]:
             min_idx = j
-<<<<<<< HEAD:selection.py
     
     nums[i], nums[min_idx] = nums[min_idx], nums[i]
     print(f"Min Number {nums[i]}")
-=======
-    A[i], A[min_idx] = A[min_idx], A[i]
->>>>>>> 10ecb4db04b2f8a65503845976913e4060ed802a:sorting functions/selection.py
 
 print ("Sorted array")
 for i in range(len(nums)):
